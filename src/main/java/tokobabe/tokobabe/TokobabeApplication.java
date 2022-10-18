339e9a727acdbd4d39eef027a14e73f29a746152
ZulfaSalsabila <zulfa130802@gmail.com>
2022-10-18 13:44:42 +0700

    Commit Project

---
+++ src/main/java/tokobabe/tokobabe/TokobabeApplication.java
@@ -0,0 +1,13 @@
+package tokobabe.tokobabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokobabeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokobabeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tokobabe/tokobabe/TokobabeApplicationTests.java
@@ -0,0 +1,13 @@
+package tokobabe.tokobabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokobabeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
